9991e5b4ab656bdaebdcf4d7f3b9de4bccb45e2b
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-12-15 23:33:23 +0100

    avanzando hasta with criteria

--- spec/sqlbuilder_spec.js
+++ spec/sqlbuilder_spec.js
@@ -1,9 +1,18 @@
 describe('SQL Builder', function(){
     describe('Builder SELECT statement', function(){
+    var b = new SQLQueryBuilder();  
         it('Just with a table name', function(){
-            var b = new SQLQueryBuilder();
             var sql = b.select().from('cocotero').build();
             expect(sql).toEqual('SELECT * FROM cocotero');
         });
+        it('With a column set', function() {
+            var sql = b.select(['a','b','c']).from('cocotero').build();
+            expect(sql).toEqual('SELECT a, b, c FROM cocotero');
+        });
+        it('With some criteria', function(){
+          var criteria = {a: 1, b: 2, c: 3};
+          var sql = b.select().from('cocotero').where(criteria).build();
+          expect(sql).toEqual('SELECT * FROM cocotero WHERE a = 1 AND b = 2 AND c = 3');
+        })
     });
 });
--- src/sqlbuilder.js
+++ src/sqlbuilder.js
@@ -1,13 +1,69 @@
 var SQLQueryBuilder = function() {
 };
-SQLQueryBuilder.prototype.select = function() {
+SQLQueryBuilder.prototype.select = function(columns) {
+    this.columns = [];
+    for(var idx in columns) {
+        this.columns.push({
+          name: columns[idx],
+          last: this.columns.length + 1 == columns.length
+        });
+    }
+    if (this.columns.length == 0) {
+        this.columns.push({name: '*', last: true});
+    }
     return this;
 };
 SQLQueryBuilder.prototype.from = function(table) {
     this.table = table;
     return this;
-}
+};
+SQLQueryBuilder.prototype.where = function(criteria) {
+    var size = 0;
+    this.criteria = [];
+    for(var col in criteria)
+      size++;
+    for(var col in criteria) {
+      this.criteria.push({
+        col: col,
+        value: criteria[col],
+        last: this.criteria.length + 1 == size
+      });
+    }
+    return this;
+};
+SQLQueryBuilder.prototype.hasCriteria = function() {
+    return this.criteria != null;
+};
 SQLQueryBuilder.prototype.build = function() {
-    return 'SELECT * FROM ' + this.table;
+    var result = 'SELECT ' + this.prepareColumnsTo() + ' FROM ' + this.table;
+    if (this.hasCriteria()) {
+      result += ' WHERE ' + this.prepareColumnsValues();
+    }
+    return result;
+};
+SQLQueryBuilder.prototype.prepareColumnsTo = function() {
+    var cols = '';
+    for (var idx = 0; idx < this.columns.length; idx++) {
+        var tmp = this.columns[idx];
+        if (tmp.last) {
+          cols += tmp.name;
+        }
+        else {
+          cols += tmp.name + ', ';
+        }
+    }
+    return cols;
+};
+SQLQueryBuilder.prototype.prepareColumnsValues = function() {
+  var result = '';
+  for (var idx = 0; idx < this.criteria.length; idx++) {
+    var tmp = this.criteria[idx];
+    if (tmp.last) {
+      result += tmp.col + ' = ' + tmp.value;
+    }
+    else {
+      result += tmp.col + ' = ' + tmp.value + ' AND ';
+    }
+  }
+  return result; 
 }
-
