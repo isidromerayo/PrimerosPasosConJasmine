fb8d4494ec9b33dda5179f55ff7cd79e0efe3418
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-12-27 18:50:04 +0100

    comenzando con INSERT

--- spec/sqlbuilder_spec.js
+++ spec/sqlbuilder_spec.js
@@ -1,6 +1,10 @@
 describe('SQL Builder', function(){
-    describe('Builder SELECT statement', function(){
-    var b = new SQLQueryBuilder();  
+    var b;
+    beforeEach(function() {
+        b = new SQLQueryBuilder();  
+    });
+
+    describe('Builds SELECT statement', function(){
         it('Just with a table name', function(){
             var sql = b.select().from('cocotero').build();
             expect(sql).toEqual('SELECT * FROM cocotero');
@@ -12,7 +16,48 @@ describe('SQL Builder', function(){
         it('With some criteria', function(){
           var criteria = {a: 1, b: 2, c: 3};
           var sql = b.select().from('cocotero').where(criteria).build();
-          expect(sql).toEqual('SELECT * FROM cocotero WHERE a = 1 AND b = 2 AND c = 3');
+          expect(sql).toEqual('SELECT * FROM cocotero ' +
+                              'WHERE a = 1 AND b = 2 AND c = 3');
+        });
+
+    });
+    
+    describe('Builds DELETE statements', function(){
+        it('Just with a table name', function(){
+            var sql = b.delete().from('cocotero').build();
+            expect(sql).toEqual('DELETE FROM cocotero');
+        });
+        it('With some criteria', function(){
+            var criteria = {a: 1, b: 2, c: 3};
+            var sql = b.delete().from('cocotero').where(criteria).build();
+            expect(sql).toEqual('DELETE FROM cocotero ' +
+                                'WHERE a = 1 AND b = 2 AND c = 3');
         })
+    });
+
+    describe('Builds UPDATE statements', function(){
+        it('Just with a table name and value map', function(){
+            var values = {a: 1, b: 2, c: 3};
+            var sql = b.update('cocotero').set(values).build();
+            expect(sql).toEqual('UPDATE cocotero SET a = 1, b = 2, c = 3');
+        });
+        it('Just with a table with criteria', function(){
+            var values = {a: 1, b: 2, c: 3};
+            var criteria = {d: 4};
+            var sql = b.update('cocotero').set(values).where(criteria).build();
+            expect(sql).toEqual('UPDATE cocotero SET ' + 
+                                'a = 1, b = 2, c = 3 ' +
+                                'WHERE ' + 
+                                'd = 4');
+        });
+    });
+
+    describe('Build INSERT statements', function(){
+      it('With a table name an value map', function(){
+            var values = {a: 1, b: 2, c: 3};
+            var criteria = {d: 4};
+            var sql = b.insert('cocotero').values(values).build();
+            expect(sql).toEqual('INSERT INTO cocotero(a, b, c) VALUES(1, 2, 3)');
+      });
     });
 });
--- src/sqlbuilder.js
+++ src/sqlbuilder.js
@@ -1,6 +1,7 @@
 var SQLQueryBuilder = function() {
 };
 SQLQueryBuilder.prototype.select = function(columns) {
+    this.operacion = 'SELECT';
     this.columns = [];
     for(var idx in columns) {
         this.columns.push({
@@ -10,6 +11,50 @@ SQLQueryBuilder.prototype.select = function(columns) {
     }
     if (this.columns.length == 0) {
         this.columns.push({name: '*', last: true});
+    }
+    return this;
+};
+SQLQueryBuilder.prototype.delete = function() {
+  this.operacion = 'DELETE';
+  this.columns = [];
+  return this;
+};
+SQLQueryBuilder.prototype.update = function(table) {
+  this.operacion = 'UPDATE';
+  this.table = table;
+  return this;
+};
+SQLQueryBuilder.prototype.insert = function(table) {
+    this.operacion = 'INSERT';
+    this.table = table;
+    return this;
+};
+SQLQueryBuilder.prototype.values = function(params) {
+    var size = 0;
+    for (var idx in params)
+        size++;
+    this.columns = [];
+    this.valores = [];
+    for(var idx in params) {
+        this.columns.push({
+          col: idx,
+          value: params[idx],
+          last: this.columns.length + 1 == size
+        });
+    }
+    return this;
+};
+SQLQueryBuilder.prototype.set = function(values) {
+    var size = 0;
+    for (var idx in values)
+        size++;
+    this.columns = [];
+    for(var idx in values) {
+        this.columns.push({
+          col: idx,
+          value: values[idx],
+          last: this.columns.length + 1 == size
+        });
     }
     return this;
 };
@@ -35,16 +80,23 @@ SQLQueryBuilder.prototype.hasCriteria = function() {
     return this.criteria != null;
 };
 SQLQueryBuilder.prototype.build = function() {
-    var result = 'SELECT ' + this.prepareColumnsTo() + ' FROM ' + this.table;
+    var criteria = '';
     if (this.hasCriteria()) {
-      result += ' WHERE ' + this.prepareColumnsValues();
+      criteria = ' WHERE ' + this.prepareColumnsValues();
     }
-    return result;
+    var tpls = {
+        SELECT:   'SELECT ' + this.prepareColumnsTo(this.columns) + ' FROM ' + this.table,
+        DELETE: 'DELETE FROM ' + this.table,
+        UPDATE: 'UPDATE ' + this.table + ' SET ' + this.preparePairColumnValue(),
+        INSERT: 'INSERT INTO ' + this.table  + this.prepareElementInsert('col') + 
+          ' VALUES' + this.prepareElementInsert('value')
+    }
+    return tpls[this.operacion] + criteria;
 };
-SQLQueryBuilder.prototype.prepareColumnsTo = function() {
+SQLQueryBuilder.prototype.prepareColumnsTo = function(args) {
     var cols = '';
-    for (var idx = 0; idx < this.columns.length; idx++) {
-        var tmp = this.columns[idx];
+    for (var idx = 0; idx < args.length; idx++) {
+        var tmp = args[idx];
         if (tmp.last) {
           cols += tmp.name;
         }
@@ -66,4 +118,33 @@ SQLQueryBuilder.prototype.prepareColumnsValues = function() {
     }
   }
   return result; 
+};
+SQLQueryBuilder.prototype.preparePairColumnValue = function() {
+  var result = '';
+  for (var idx = 0; idx < this.columns.length; idx++) {
+    var tmp = this.columns[idx];
+    if (tmp.last) {
+      result += tmp.col + ' = ' + tmp.value;
+    }
+    else {
+      result += tmp.col + ' = ' + tmp.value + ', ';
+    }
+  }
+  return result; 
+
+};
+SQLQueryBuilder.prototype.prepareElementInsert = function(element) {
+  var result='';
+  result +='(';
+  for (var idx = 0; idx < this.columns.length; idx++) {
+    var tmp = this.columns[idx];
+    if (tmp.last) {
+      result += tmp[element] ;
+    }
+    else {
+      result += tmp[element] + ', ';
+    }
+  }
+  result +=')';
+  return result;
 }
